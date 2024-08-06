21b42fbd11a09349ed14ccaa48386168d11ae259
ayunifebila <145086991+ayunifebila@users.noreply.github.com>
2024-08-06 09:33:00 +0700

    Add files via upload

---
+++ lthn2.php
@@ -0,0 +1,14 @@
+<?php
+
+$nrp = "12345678910";
+$nama = "loren ipsum";
+$umur = 20;
+$nilai = 75.75;
+
+echo "Nrp   : ".$nrp . "</br>";
+echo "Nama  : ".$nama . "</br>";
+print "Umur : ".$umur; print "</br>";
+// menampilkan bilangan real dengan format 3 digit dibelakang koma
+printf ("Nilai : %.3f</br>", $nilai);
+
+?>
---
+++ variabel_operator.php
@@ -0,0 +1,18 @@
+<?php
+
+//menetapkan nilai
+$x=21;
+$y=3;
+
+echo "x = $x</br>";
+echo "y = $y</br>";
+
+//pembagian
+$hasilBagi = $x/$y;//Tanda / adalah operator
+echo "Hasil bagi dari x/ y = $hasilBagi </br>
+
+//Perkalian
+$hasilKali = $x * $y; //Tanda * adalah operator
+echo "Hasil bagi dari x * y = $hasilKali </br>";
+
+?>
